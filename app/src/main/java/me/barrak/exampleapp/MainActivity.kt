391411e5749b03b0cad14a39c38869e66b9645de
Ali Albarrak <alaa13212@gmail.com>
2021-03-26 12:16:05 +0300

    Added  Lazy List and dynamic button state

--- app/src/main/java/me/barrak/exampleapp/MainActivity.kt
+++ app/src/main/java/me/barrak/exampleapp/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,24 +32,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     AndroidExampleAppTheme {
-        Surface(color = Color.Yellow, modifier = Modifier.fillMaxHeight()) {
+        Surface(color = Color.Yellow) {
             content()
         }
     }
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
     val counterState = remember { mutableStateOf(0) }
 
-    Column {
-        for (name in names) {
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names = names, modifier = Modifier.weight(1f))
+        Counter(count = counterState.value, updateCount = { newCount -> counterState.value = newCount })
+    }
+}
+
+@Composable
+private fun NameList(names: List<String>, modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
+        items (items = names) { name ->
             Greeting(name = name)
             Divider(color = Color.Black)
         }
-
-        Divider(color = Color.Transparent, thickness = 32.dp)
-        Counter(count = counterState.value, updateCount = { newCount -> counterState.value = newCount })
     }
 }
 
@@ -61,7 +65,12 @@ fun Greeting(name: String) {
 
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(onClick = { updateCount(count + 1) }) {
+    Button(
+        onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if(count > 5) Color.Green else Color.White
+        )
+    ) {
         Text("I've been clicked $count times")
     }
 }
