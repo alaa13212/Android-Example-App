16b5d61e8547c4397646283c802fe966a7b21765
Ali Albarrak <alaa13212@gmail.com>
2021-03-26 13:53:56 +0300

    Scaffold app top bar

---
+++ app/src/main/java/me/barrak/exampleapp/ui/components/MyAppTopBar.kt
@@ -0,0 +1,32 @@
+package me.barrak.exampleapp.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.tooling.preview.*
+import me.barrak.exampleapp.ui.*
+
+@Composable
+fun MyAppTopBar(title: String = "Example App") {
+    TopAppBar(
+        title = {
+            Text(text = title, maxLines = 2)
+        },
+        navigationIcon = {
+            Icon(Icons.Rounded.Menu, contentDescription = null, modifier = Modifier.fillMaxWidth())
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun MyAppTopBarPreview() {
+    MyApp {
+        MyAppTopBar("Screen Title")
+    }
+}
+
--- app/src/main/java/me/barrak/exampleapp/ui/screens/MyScreenContent.kt
+++ app/src/main/java/me/barrak/exampleapp/ui/screens/MyScreenContent.kt
@@ -1,11 +1,9 @@
 package me.barrak.exampleapp.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.tooling.preview.*
-import me.barrak.exampleapp.*
 import me.barrak.exampleapp.ui.*
 import me.barrak.exampleapp.ui.components.*
 
@@ -14,6 +12,7 @@ fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
     val counterState = remember { mutableStateOf(0) }
 
     Column(modifier = Modifier.fillMaxHeight()) {
+        MyAppTopBar()
         NameList(names = names, modifier = Modifier.weight(1f))
         Counter(count = counterState.value, updateCount = { newCount -> counterState.value = newCount })
     }
