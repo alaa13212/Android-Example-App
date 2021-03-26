255596f97615e77e3173be2abf7006374f106c4c
Ali Albarrak <alaa13212@gmail.com>
2021-03-26 11:51:12 +0300

    Add Counter Component

--- app/src/main/java/me/barrak/exampleapp/MainActivity.kt
+++ app/src/main/java/me/barrak/exampleapp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     AndroidExampleAppTheme {
-        Surface(color = Color.Yellow) {
+        Surface(color = Color.Yellow, modifier = Modifier.fillMaxHeight()) {
             content()
         }
     }
@@ -37,17 +41,29 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
+
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(count = counterState.value, updateCount = { counterState.value++ })
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: () -> Unit) {
+    Button(onClick = updateCount) {
+        Text("I've been clicked $count times")
+    }
 }
 
 @Preview(showBackground = true, name = "Text preview")
